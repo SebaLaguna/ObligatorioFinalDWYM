a13810b89f689501ddbf904481255cc61e8e04dc
Santiago García <santi.garcia3@hotmail.com>
2024-11-06 20:49:21 -0300

    Logica del like terminada
    El fetch para agregar y quitar un like esta terminado. Falta que el cambio de numero se vea sin tener que refrescar la pagina a mano

--- Frontend/src/components/Publicacion.jsx
+++ Frontend/src/components/Publicacion.jsx
@@ -2,6 +2,38 @@ import "../styles/FeedPage.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Comment from "./Comment";
 
+const darLike = async(post) => {
+  try{
+    const response = await fetch(`http://localhost:3001/api/posts/${post._id}/like`,{
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Error al dar un like');
+    }
+  } catch (error){
+    console.error("Error al dar un like", error);
+  }
+}
+
+const quitarLike = async(post) => {
+  try{
+    const response = await fetch(`http://localhost:3001/api/posts/${post._id}/like`,{
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Error al quitar un like');
+    }
+  }catch (error){
+    console.error("Error al quitar un like",error)
+  }
+}
+
 const Publicacion = ({selectedPost}) => {
     return(
             <div>
@@ -9,7 +41,10 @@ const Publicacion = ({selectedPost}) => {
               src={selectedPost.imageUrl}
               alt="Post"
               className="modal-post-image"
+              onClick={() => darLike(selectedPost)}
             />
+            <button onClick={()=> darLike(selectedPost)}> Dar Like </button>
+            <button onClick={()=> quitarLike(selectedPost)}> Quitar Like </button>
             <div className="modal-caption">
               <p>{selectedPost.caption}</p>
             </div>
