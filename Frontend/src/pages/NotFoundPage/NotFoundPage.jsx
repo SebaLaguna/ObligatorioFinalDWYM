c27f6921db23ebb25684cbab5ecb32ee10a0001a
Sebastian <sebastianlagunamatiaude@gmail.com>
2024-11-26 13:28:56 -0300

    .

--- Frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ Frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/NotFoundPage.css";
+
 const NotFoundPage = () => {
-    return (
-      <div>
-        <h2>404 - Page Not Found</h2>
-        <p>Oops! The page you're looking for doesn't exist.</p>
-      </div>
-    );
-  };
-  
-  export default NotFoundPage;
-  
+  return (
+    <div className="not-found-page">
+      <h2>404 - Page Not Found</h2>
+      <p>Oops! The page you're looking for doesn't exist.</p>
+      <Link to="/" className="go-home-button">
+        Go to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
